85771000b0bd3a97db330a4679d51beb3483cc61
NikolayCheremnov <coolnick1999@gmail.com>
2024-09-17 23:01:46 +0700

    Добавлен метод IsSimple и CLI-оболочка

--- GitExample_PV324/Program.cs
+++ GitExample_PV324/Program.cs
@@ -8,9 +8,34 @@ namespace GitExample_PV324
 {
     internal class Program
     {
+        // IsSimple - функция определения того, является ли число простым или составным
+        // вход: number - целое число
+        // выход: true если модуль number - простое число, иначе false
+        // примечание: 1, 0 считаются как простые
+        static bool IsSimple(int number)
+        {
+            number = Math.Abs(number);
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from git project!");
+            Console.Write("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool isSimple = IsSimple(n);
+            if (isSimple) {
+                Console.WriteLine($"{n} is simple");
+            } else
+            {
+                Console.WriteLine($"{n} is composite number");
+            }
         }
     }
 }
